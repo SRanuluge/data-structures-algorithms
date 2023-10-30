c6ae8686a2b546f30644878d37603413afa2b1ed
Sanjeewa Ranuluge <sranuluge.dev@gmail.com>
2023-10-31 00:05:57 +1300

    feat: added problem and solution

---
+++ src/sranuluge/python/intersection-multiple-array/method1.py
@@ -0,0 +1,11 @@
+
+def intersection(nums):
+    new_set = set(nums[0])
+    for i in range(1, len(nums)):
+        new_set &= set(nums[i])
+    new_set = list(new_set)
+    new_set.sort()
+    return new_set
+
+array = [[3,1,2,4,5],[1,2,3,4],[3,4,5,6]]
+print(intersection(array))
